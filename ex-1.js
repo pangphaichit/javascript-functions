4edfb8e7acc1a6a4c520da046374b395223cc7ab
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-10-03 15:40:50 +0700

    feat:assigment2

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,34 @@
-// Start coding here
+// ให้เขียนโปรแกรมตามโจทย์ที่กำหนดดังนี้
+
+// ให้ Declare ฟังก์ชัน multiply ลงในโค้ดโดยที่
+
+// Declare ฟังก์ชัน multiply ในรูปแบบของ Declaration
+// ฟังก์ชัน multiply รับ Parameter 2 ตัวคือ a มีชนิดข้อมูลเป็น Number และ b มีชนิดข้อมูลเป็น Number
+// ฟังก์ชัน multiply จะ Return ผลลัพธ์ออกมาเป็น ผลคูณระหว่าง a และ b
+// เช่น ถ้า Execute multiply(10, 5) จะได้ผลลัพธ์ออกมาเป็น 50
+// ให้ Declare ฟังก์ชัน divide ลงในโค้ดโดยที่
+
+// Declare ฟังก์ชัน divide ในรูปแบบของ Declaration
+// ฟังก์ชัน divide รับ Parameter 2 ตัวคือ a มีชนิดข้อมูลเป็น Number และ b มีชนิดข้อมูลเป็น Number
+// ฟังก์ชัน divide จะ Return ผลลัพธ์ออกมาเป็น ผลหารระหว่าง a และ b
+// เช่น ถ้า Execute divide(10, 5) จะได้ผลลัพธ์ออกมาเป็น 2// Start coding here
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(10, 5));
+
+function subtract(a, b) {
+  return a - b;
+}
+console.log(subtract(10, 5));
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply(10, 5));
+
+function divide(a, b) {
+  return a / b;
+}
+console.log(divide(10, 5));
